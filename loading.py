6a0a444ad26c0e29f212093a38f7828548806712
Kinjal Amit Heda <160608303+Kinjalah@users.noreply.github.com>
2025-12-22 15:31:16 +0530

    Delete loading.py

--- loading.py
+++
@@ -1,7 +0,0 @@
-import nibabel as nib
-
-# Load .nii file
-img = nib.load("BraTS20_Training_001_seg (1).nii")   # replace with your file path
-data = img.get_fdata()
-
-print("Image shape:", data.shape)   # e.g., (256, 256, 150)
